5d8fbbc01ca09c033f1df8257c2d5064aae60dee
Alexander Raievskyi <araievskyi@gmail.com>
2020-02-19 02:25:35 +0200

    :truck: update(general-statistic): rename component & delete redundant html-file

---
+++ src/app/statistic-pane/statistic.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statistic',
+  template: `
+        <span>Stats work!</span>
+  `,
+  styleUrls: ['./statistic.component.scss']
+})
+export class StatisticComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
